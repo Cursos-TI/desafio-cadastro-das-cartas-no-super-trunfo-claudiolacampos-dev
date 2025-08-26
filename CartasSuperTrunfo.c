791a424b65f8b58d91da4ab0c645abcf38984317
claudiolacampos-dev <claudiolacampos@gmail.com>
2025-08-26 20:21:16 +0000

    Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,6 +25,8 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
+    float densidade1;
+    float pibPerCapita1;
 
     // Variáveis da Carta 2
     char estado2;
@@ -34,6 +36,9 @@ int main() {
     float area2;
     float pib2;
     int pontosTuristicos2;
+    float densidade2;
+    float pibPerCapita2;
+
 
     // ===== Cadastro da Carta 1 =====
     printf("Cadastro da Carta 1:\n");
@@ -57,6 +62,12 @@ int main() {
 
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos1);
+   
+    // Calculos da Carta 1
+
+    densidade1 = populacao1 / area1;
+    pibPerCapita1 = (pib1 *1000000000) / populacao1;
+    // multiplica PIB por 1 bilhão para transformar em reais
 
     // ===== Cadastro da Carta 2 =====
     printf("\nCadastro da Carta 2:\n");
@@ -81,6 +92,11 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
+    // Calculos da Carta 2
+
+    densidade2 = populacao2 / area2;
+    pibPerCapita2 = (pib2 *1000000000) / populacao2;
+
       // ===== Exibição dos dados =====
     printf("\n=== Carta 1 ===\n");
     printf("Estado: %c\n", estado1);
@@ -90,6 +106,9 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais \n", pibPerCapita1);
+
 
     printf("\n=== Carta 2 ===\n");
     printf("Estado: %c\n", estado2);
@@ -99,6 +118,9 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais \n", pibPerCapita2);
+
 
 
 
